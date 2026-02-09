ac6c570cfc32b9c6e0f2675491c936422d224e68
joaohenriqueoldm-hub <joaohenriqueoldm@gmail.com>
2026-02-09 15:38:29 -0300

    Q45_saque.py

--- Q45_saque.py
+++ Q45_saque.py
@@ -11,4 +11,5 @@ nota_de_2 = ((saque % 10) % 5) // 2
 moeda_de_1 = (((saque % 10) % 5 ) % 2)
 
 # Exibir o resultado
-print(f"R${saque} sacados em {nota_de_100} notas de 100, {nota_de_50} de 50, {nota_de_20} de 20, {nota_de_10} de 10, {nota_de_5} de 5, {nota_de_2} de 2, {moeda_de_1} moedas de 1 real")
+
+print(f"R${saque} serão sacados em {nota_de_100} notas de 100, {nota_de_50} de 50, {nota_de_20} de 20, {nota_de_10} de 10, {nota_de_5} de 5, {nota_de_2} de 2, {moeda_de_1} moedas de 1 real")

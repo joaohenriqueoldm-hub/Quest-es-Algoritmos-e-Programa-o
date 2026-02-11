ddb86222d73c579b32be8f9b7fff84039d5f5ad5
joaohenriqueoldm-hub <joaohenriqueoldm@gmail.com>
2026-02-11 19:23:37 -0300

    Q31_numero_binario.py

--- Q31_numero_binario.py
+++ Q31_numero_binario.py
@@ -2,8 +2,14 @@
 binario = input("Digite um número binário de quatro dígitos:")
 
 # Processamento - convertendo de binário para a base decimal
-decimal = int(binario, 2)
+numero_1 = ((binario // 1000)) * 8
+numero_2 = ((binario % 1000) // 100) * 4
+numero_3 = (((binario % 1000) % 100) // 10) * 2
+numero_4 = (((binario % 1000) % 100) % 10) * 1
+
+decimal = numero_1 + numero_2 + numero_3 + numero_4
 
 # Exibir o resultado
 print(f"O número binário {binario} é equivalente a {decimal} na base decimal ")
+
 
